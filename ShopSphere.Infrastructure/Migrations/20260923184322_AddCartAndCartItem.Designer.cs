d77dc8af76d530288ad3d4be6d4862fb7ee76ff4
Chiheb <chihebabiza@gmail.com>
2026-09-23 19:43:46 +0100

    apply migrations

--- ShopSphere.Infrastructure/Migrations/20260923184322_AddCartAndCartItem.Designer.cs
+++ ShopSphere.Infrastructure/Migrations/20260923184322_AddCartAndCartItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopSphere.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using ShopSphere.Infrastructure.Persistence;
 namespace ShopSphere.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260923184322_AddCartAndCartItem")]
+    partial class AddCartAndCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
